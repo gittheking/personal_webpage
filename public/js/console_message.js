6200a93f3055f685c0f1288b61d12e5aea410792
Bobby King <king.robertjoseph@gmail.com>
2017-01-01 22:38:07 -0500

    Add console_message.js

---
+++ public/js/console_message.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(() => {
+  const welcome = '__        __   _                          _\n'
+                + '\\ \\      / /__| | ___ ___  _ __ ___   ___| |\n'
+                + ' \\ \\ /\\ / / _ \\ |/ __/ _ \\| \'_ ` _ \\ / _ \\ |\n'
+                + '  \\ V  V /  __/ | (_| (_) | | | | | |  __/_|\n'
+                + '   \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___(_)'
+                + '\nI see you like to checkout the console as well, eh?\n'
+                + 'If you\'d like to checkout some more of my code head on over to http://github.com/gittheking\n'
+                + 'I\'m always looking for fun new projects to work on so if you\'re interested in collaborating,'
+                + ' please feel free to reach out to me at king.robertjoseph@gmail.com';
+
+  console.log(welcome);
+})();
